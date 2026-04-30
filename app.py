34b6336c9dcdc30f765498b0288a098c5ae94287
CoderVarun765 <varungup765@gmail.com>
2026-04-30 14:44:45 +0530

    fix: PORT fix

--- app.py
+++ app.py
@@ -55,6 +55,4 @@ def predict():
         "verification": verification
     })
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+port = int(os.environ.get("PORT", 5000))
